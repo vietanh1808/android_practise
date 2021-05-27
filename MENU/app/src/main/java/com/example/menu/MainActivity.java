b3a54b0fca86e6e6b1d9a95cd6be6864246abc51
unknown <vietanhnguyen1808@gmail.com>
2021-05-27 22:42:53 +0700

    Menu and Alert Dialog

--- MENU/app/src/main/java/com/example/menu/MainActivity.java
+++ MENU/app/src/main/java/com/example/menu/MainActivity.java
@@ -1,62 +1,51 @@
 package com.example.menu;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txtHello;
     ConstraintLayout constraintLayout;
+    Button btnConfirm, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtHello = (TextView) findViewById(R.id.txtHello);
-        txtHello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopupMenu();
-            }
-        });
+        mapping();
 
-        constraintLayout = (ConstraintLayout) findViewById(R.id.layoutMain);
+        txtHello.setOnClickListener(this);
+        btnConfirm.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
         this.registerForContextMenu(constraintLayout);
+
     }
 
-    // Hiện menu với sự kiện Button
-    private void showPopupMenu() {
-        PopupMenu popupMenu = new PopupMenu(this, txtHello);
-        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.mnuOne:
-                        Toast.makeText(MainActivity.this, "Bạn đã chọn 1", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.mnuTwo:
-                        Toast.makeText(MainActivity.this, "Bạn đã chọn 2", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.mnuThree:
-                        Toast.makeText(MainActivity.this, "Bạn đã chọn 3", Toast.LENGTH_LONG).show();
-                        break;
-                }
-                return false;
-            }
-        });
-        popupMenu.show();
+    private void mapping() {
+        constraintLayout = findViewById(R.id.layoutMain);
+        txtHello = findViewById(R.id.txtHello);
+        btnConfirm = findViewById(R.id.btnConfirm);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 
     // Hiển thị Menu ở nền
@@ -124,5 +113,93 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnConfirm:
+                showConfirm();
+                break;
+            case R.id.txtHello:
+                showPopupMenu();
+                break;
+            case R.id.btnLogin:
+                showDialogLogin();
+        }
+    }
+
+    // Hàm hiện menu với sự kiện Button
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(this, txtHello);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mnuOne:
+                        Toast.makeText(MainActivity.this, "Bạn đã chọn 1", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.mnuTwo:
+                        Toast.makeText(MainActivity.this, "Bạn đã chọn 2", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.mnuThree:
+                        Toast.makeText(MainActivity.this, "Bạn đã chọn 3", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+    private void showConfirm() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cảnh báo!!");
+        builder.setMessage("Đây là thông báo xác nhận ...");
+        builder.setIcon(R.mipmap.ic_launcher);
+        // thiết lập 2 nút khác nhau
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Bạn đã click vào OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Bạn đã click vào Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+    private void showDialogLogin() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.login);
+        dialog.setTitle("CustomDialog");
+        dialog.setCanceledOnTouchOutside(false);
+
+        EditText edtUsername = dialog.findViewById(R.id.edtUsername);
+        EditText edtPassword = dialog.findViewById(R.id.edtPassword);
+        Button btnLoginDialog = dialog.findViewById(R.id.btnLoginDialog);
+        Button btnCancelDialog = dialog.findViewById(R.id.btnCancelDialog);
+
+        btnCancelDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnLoginDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                if (username.equals("admin") && password.equals("admin")) {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        dialog.show();
     }
 }
